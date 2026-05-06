bc113a5af95087e771586228de9df555613d20a4
millene289 <millene.daniel.mendonca@escola.pr.gov.br>
2026-05-06 12:14:39 -0300

    j

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll (".botao");
 for (let i = 0; i < botoes.length; i++){
         botoes[i].onclik = function () {
         for (let j = 0; j < botoes.length; j++) {
-        botoes[j].classlist.remove("ativo");   
+        botoes[j].classList.remove("ativo");   
         }
     
     botoes[i].classList.add("ativo");
